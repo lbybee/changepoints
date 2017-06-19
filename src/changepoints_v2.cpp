a4ab21f866c6c32760e454839894b1652a8b0fd6
lbybee <leland.bybee@gmail.com>
2017-06-19 18:42:49 -0500

    add model outline code

--- src/changepoints_v2.cpp
+++ src/changepoints_v2.cpp
@@ -3,3 +3,145 @@
 using namespace std;
 using namespace Rcpp;
 using namespace arma;
+
+
+// TODO document
+// [[Rcpp::export]]
+mat mapping(mat cov_est, mat theta_start, double update_w,
+            double update_change, double regularizer, int max_iter
+            double tol){
+    /* Produces the regularized estimation of the covariance matrix using
+     * the proximal gradient procedure described in
+     *
+     * http://dept.stat.lsa.umich.edu/~yvesa/sto_prox.pdf
+     *
+     * Parameters
+     * ----------
+     *
+     *  cov_est : mat
+     *      The covariance of the data for the current tau values, should
+     *      be P x P.
+     *  theta_start : mat
+     *      The starting value for theta, should be P x P.
+     *  update_w : double
+     *      The weight for the update to theta, gamma in the source paper.
+     *  update_change : double
+     *      Change to update_w when estimation procedure fails.  New
+     *      update_w *= update_change.
+     *      The amount by which update_w shrinks with each failed run
+     *  regularizer : double
+     *      Regularizing constant, lamb in source paper.
+     *  max_iter : int
+     *      Limits the maximum number iterations.
+     *  tol : double
+     *      The tolerance at which updates are stopped.
+     *
+     * Returns
+     * -------
+     *
+     *  theta_p : mat
+     *      current estimate for theta.
+     */
+
+    int P = cov_est.n_cols;
+
+    // TODO may not need to fill theta_p and inv_theta with 0s
+    // proposed theta estimate
+    mat theta_p = eye<mat>(P, P);
+    // current theta estimate
+    mat theta_k = theta_start;
+    mat inv_theta = eye<mat>(P, P);
+
+    int i = 0;
+
+    float delta_norm = norm(theta_k - theta_p) / norm(theta_k);
+
+    bool state = false;
+
+    while (state and i < max_iter) {
+
+        try {
+            inv_theta = inv(theta_k);
+            theta_p = theta_k - update_w * (cov_est - inv_theta);
+            for (int j = 0; j < P; j++) {
+                for (int k = 0; k < P; k++) {
+                    if (theta_p(j,k) <= -regularizer) {
+                        theta_p(j,k) += regularizer;
+                    }
+                    else if (theta_p(j,k) >= regularizer) {
+                        theta_p(j,k) -= regularizer;
+                    }
+                    else {
+                        theta_p(j,k) = 0;
+                    }
+                }
+            }
+        }
+        // TODO add correct error here, this is sloppy
+        catch(...) {
+            update_w *= update_change;
+            theta_k = theta_start;
+        }
+        delta_norm = norm(theta_k - theta_p) / norm(theta_k);
+        theta_k = theta_p;
+        if (delta_norm < tol) {
+            state = false;
+        }
+        i += 1;
+    }
+
+    return theta_p;
+}
+
+
+// TODO document
+// [[Rcpp::export]]
+double gg_log_likelihood(mat data, mat theta_i, double regularizer) {
+    /* Generates the log-likelihood for the specified theta and
+     * tau values
+     *
+     * Parameters
+     * ----------
+     *
+     *  data : mat
+     *      N x P matrix containing the data used for estimation
+     *  theta_i : mat
+     *      P x P inverse cov estimate for first partition
+     *  regularizer : double
+     *      regularizing constant
+     *
+     * Returns
+     * -------
+     *
+     *  double corresponding to log-likelihood
+     *
+     * Notes
+     * -----
+     *
+     *  - {N_tau / 2 * [tr(theta_i.T * S_i) - logdet(theta_i)]
+     *     + lambda_tau * || theta_i || / 2}
+     *  We take the negative here because we are minimizing
+     *  this. TODO confirm that everything lines up
+     *
+     *  Also, we divide the regularizer by 2 so that we can
+     *  include one in both likelihood (and not have to
+     *  have this as external code).
+     */
+
+    int N = data.n_rows;
+    int P = data.n_cols;
+
+    // new
+    mat S = cov(data);
+
+    mat TdS = theta_i % S;
+    double tr_TdS = trace(TdS);
+
+    double sign;
+    double det; log_det(det, sign, theta_i);
+
+    double ll = N * 0.5 * (-det + tr_TdS);
+    ll += regularizer * sqrt(log(P) / (N)) * norm(theta_i, 1) * 0.5;
+
+    return -ll;
+}
--- src/changepoints_v2.h
+++ src/changepoints_v2.h
@@ -4,6 +4,16 @@ using namespace std;
 using namespace Rcpp;
 using namespace arma;
 
+// Note that the mapping and gg_loglikelihood functions are included
+// primarily for testing purposes and to provide an example use case
+//
+// mapping function
+mat mapping(mat cov_est, mat theta_start, double update_w,
+            double update_change, double regularizer, int max_iter,
+            double tol);
+// gaussian graphical model log-likelihood
+double gg_loglikelihood(mat data, mat theta_i, double regularizer);
+
 
 // changepoint model class
 class ChangepointModel {
@@ -12,25 +22,25 @@ class ChangepointModel {
         // data
         mat data; // the data used for estimating the model, N x K
 
-        // method params
-        List method_params; // the different methods require different params
-                            // and these are contained here
-
-        // changepoint estimation params
-        vec changepoints; // arbitrary length vector of changepoints, includes
-                          // end points (0, N)
-
         // bbmod specific vars
         List bbmod_est; // arbitrary List containing black box model estimates
         List bbmod_params; // black box model specific params
+        List bbmod_ll_params; // black box model log-likelihood params
 
         // bbmod functions
+        // TODO we should ultimately overload these so people can use native
+        // Cpp functions as well
+        //
+        // Currently both of these should return Lists TODO change this
         Function bbmod_method; // Function used to generate bbmod_est
         Function bbmod_ll; // Function used to estimate bbmod log likelihood
 
         // ------------ functions ------------
-        void init(mat dataS, List bbmod_params,
-        int simulated_annealing();
-        int brute_force();
-        int rank_one();
-        vec binary_segmentation();
+        void init(mat dataS, List bbmod_paramsS, List bbmod_ll_params,
+                  Function bbmod_methodS, bbmod_llS)
+        int simulated_annealing(int niter=500);
+        int brute_force(int niter=500);
+        int rank_one(int niter=500);
+        vec binary_segmentation(int method_ind, List method_params,
+                                double threshold);
+}
